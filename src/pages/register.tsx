f9b7a64029cb3d657ed6de14271f1d469ccbca3d
Cheng Peng <pengchengkevin@hotmail.com>
2020-11-12 17:41:51 -0800

    add email, forgot password  wip

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -17,10 +17,10 @@ const Register: React.FC<registerProps> = ({}) => {
   return (
     <Wrapper variant="small">
       <Formik
-        initialValues={{ username: "", password: "" }}
+        initialValues={{ email: "", username: "", password: "" }}
         onSubmit={async (values, { setErrors }) => {
           console.log(values);
-          const response = await register(values);
+          const response = await register({ options: values });
           if (response.data?.register.errors) {
             setErrors(toErrorMap(response.data.register.errors));
           } else if (response.data?.register.user) {
@@ -41,6 +41,14 @@ const Register: React.FC<registerProps> = ({}) => {
                 placeholder="password"
                 label="Password"
                 type="password"
+              />
+            </Box>
+            <Box mt={4}>
+              <InputField
+                name="email"
+                placeholder="email"
+                label="Email"
+                type="email"
               />
             </Box>
             <Button mt={4} isLoading={isSubmitting} color="green" type="submit">
